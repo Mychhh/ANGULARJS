6cd143304885f5dcb0a2d648c1330e5d2478590b
Michael <michaeldrpabaya@gmail.com>
2023-04-02 13:53:24 +0800

    back to start

--- fake-data/src/app/app.module.ts
+++ fake-data/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FakeDataModule } from './fake-data/fake-data.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +11,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FakeDataModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
