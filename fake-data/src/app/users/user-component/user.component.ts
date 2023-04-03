4d051b17bfbd50459df4e72f2f96569f51a8f53f
Michael <michaeldrpabaya@gmail.com>
2023-04-03 12:01:28 +0800

    parent to child user

---
+++ fake-data/src/app/users/user-component/user.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Users } from 'src/app/interface/users';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+
+  @Input() fakeUserData: Users = {} as Users;
+
+}
--- fake-data/src/app/users/users.module.ts
+++ fake-data/src/app/users/users.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UserContainerComponent } from '../container/user-container.component';
 import { UsersComponent } from './users-component/users.component'
+import { NavigationComponent } from '../semantics/navigation.component';
+import { UserComponent } from './user-component/user.component';
 
 @NgModule({
   declarations: [
     UserContainerComponent,
+    NavigationComponent,
     UsersComponent,
+    UserComponent,
   ],
   imports: [
     CommonModule,
